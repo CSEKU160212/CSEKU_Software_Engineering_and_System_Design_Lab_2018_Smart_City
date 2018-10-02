af6ee7011fe82aa63843716dabf9a415b45872fd
nilrahman <42181040+nilrahman@users.noreply.github.com>
2018-10-02 18:51:09 +0600

    UI Design & Navigation Between UI

---
+++ SMTM/UI Design With Navigation/SMARTMOVIETHEATERMANAGEMENT/SMARTMOVIETHEATERMANAGEMENT/Add_Movie.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SMARTMOVIETHEATERMANAGEMENT
+{
+    public partial class Add_Movie : Form
+    {
+        public Add_Movie()
+        {
+            InitializeComponent();
+        }
+
+        private void Manage_Theater_Panel_Click(object sender, EventArgs e)
+        {
+            Manage_Theater Manage_Theater = new Manage_Theater();
+            Manage_Theater.Show();
+            this.Hide();
+        }
+
+        private void Add_Movie_Button_Click(object sender, EventArgs e)
+        {
+            Manage_Theater Manage_Theater = new Manage_Theater();
+            Manage_Theater.Show();
+            this.Hide();
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Movie_Name_OnValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Description_OnValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Movie_Image_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ SMTM/UI Design With Navigation/SMARTMOVIETHEATERMANAGEMENT/SMARTMOVIETHEATERMANAGEMENT/User_Login.Designer.cs
@@ -0,0 +1,282 @@
+﻿namespace SMARTMOVIETHEATERMANAGEMENT
+{
+    partial class User_Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(User_Login));
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Admin_Login_Button = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.User_Login_Button = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.Exit_Button = new Bunifu.Framework.UI.BunifuImageButton();
+            this.Password = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.User_Name = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.Welcome_Text = new Bunifu.Framework.UI.BunifuMetroTextbox();
+            this.Login_Button = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.Password_Icon = new System.Windows.Forms.PictureBox();
+            this.User_Icon = new System.Windows.Forms.PictureBox();
+            this.Main_Logo = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Exit_Button)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Password_Icon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.User_Icon)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Main_Logo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Admin_Login_Button
+            // 
+            this.Admin_Login_Button.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.Admin_Login_Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.Admin_Login_Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Admin_Login_Button.BorderRadius = 0;
+            this.Admin_Login_Button.ButtonText = "Admin Login";
+            this.Admin_Login_Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Admin_Login_Button.DisabledColor = System.Drawing.Color.Gray;
+            this.Admin_Login_Button.Iconcolor = System.Drawing.Color.Transparent;
+            this.Admin_Login_Button.Iconimage = null;
+            this.Admin_Login_Button.Iconimage_right = null;
+            this.Admin_Login_Button.Iconimage_right_Selected = null;
+            this.Admin_Login_Button.Iconimage_Selected = null;
+            this.Admin_Login_Button.IconMarginLeft = 0;
+            this.Admin_Login_Button.IconMarginRight = 0;
+            this.Admin_Login_Button.IconRightVisible = true;
+            this.Admin_Login_Button.IconRightZoom = 0D;
+            this.Admin_Login_Button.IconVisible = true;
+            this.Admin_Login_Button.IconZoom = 90D;
+            this.Admin_Login_Button.IsTab = false;
+            this.Admin_Login_Button.Location = new System.Drawing.Point(536, 604);
+            this.Admin_Login_Button.Name = "Admin_Login_Button";
+            this.Admin_Login_Button.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.Admin_Login_Button.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.Admin_Login_Button.OnHoverTextColor = System.Drawing.Color.LightSalmon;
+            this.Admin_Login_Button.selected = false;
+            this.Admin_Login_Button.Size = new System.Drawing.Size(229, 48);
+            this.Admin_Login_Button.TabIndex = 28;
+            this.Admin_Login_Button.Text = "Admin Login";
+            this.Admin_Login_Button.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Admin_Login_Button.Textcolor = System.Drawing.Color.White;
+            this.Admin_Login_Button.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Admin_Login_Button.Click += new System.EventHandler(this.Admin_Login_Button_Click);
+            // 
+            // User_Login_Button
+            // 
+            this.User_Login_Button.BorderColorFocused = System.Drawing.Color.White;
+            this.User_Login_Button.BorderColorIdle = System.Drawing.Color.White;
+            this.User_Login_Button.BorderColorMouseHover = System.Drawing.Color.White;
+            this.User_Login_Button.BorderThickness = 3;
+            this.User_Login_Button.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.User_Login_Button.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.User_Login_Button.ForeColor = System.Drawing.Color.White;
+            this.User_Login_Button.isPassword = false;
+            this.User_Login_Button.Location = new System.Drawing.Point(431, 336);
+            this.User_Login_Button.Margin = new System.Windows.Forms.Padding(7);
+            this.User_Login_Button.Name = "User_Login_Button";
+            this.User_Login_Button.Size = new System.Drawing.Size(437, 50);
+            this.User_Login_Button.TabIndex = 27;
+            this.User_Login_Button.Text = "User Login";
+            this.User_Login_Button.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Exit_Button
+            // 
+            this.Exit_Button.BackColor = System.Drawing.Color.SeaGreen;
+            this.Exit_Button.Image = ((System.Drawing.Image)(resources.GetObject("Exit_Button.Image")));
+            this.Exit_Button.ImageActive = null;
+            this.Exit_Button.Location = new System.Drawing.Point(1204, 12);
+            this.Exit_Button.Name = "Exit_Button";
+            this.Exit_Button.Size = new System.Drawing.Size(49, 48);
+            this.Exit_Button.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Exit_Button.TabIndex = 26;
+            this.Exit_Button.TabStop = false;
+            this.Exit_Button.Zoom = 10;
+            this.Exit_Button.Click += new System.EventHandler(this.Exit_Button_Click);
+            // 
+            // Password
+            // 
+            this.Password.BorderColorFocused = System.Drawing.Color.White;
+            this.Password.BorderColorIdle = System.Drawing.Color.White;
+            this.Password.BorderColorMouseHover = System.Drawing.Color.White;
+            this.Password.BorderThickness = 3;
+            this.Password.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Password.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.Password.ForeColor = System.Drawing.Color.White;
+            this.Password.isPassword = true;
+            this.Password.Location = new System.Drawing.Point(498, 469);
+            this.Password.Margin = new System.Windows.Forms.Padding(4);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(370, 54);
+            this.Password.TabIndex = 25;
+            this.Password.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // User_Name
+            // 
+            this.User_Name.BorderColorFocused = System.Drawing.Color.White;
+            this.User_Name.BorderColorIdle = System.Drawing.Color.White;
+            this.User_Name.BorderColorMouseHover = System.Drawing.Color.White;
+            this.User_Name.BorderThickness = 3;
+            this.User_Name.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.User_Name.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F);
+            this.User_Name.ForeColor = System.Drawing.Color.White;
+            this.User_Name.isPassword = false;
+            this.User_Name.Location = new System.Drawing.Point(498, 397);
+            this.User_Name.Margin = new System.Windows.Forms.Padding(4);
+            this.User_Name.Name = "User_Name";
+            this.User_Name.Size = new System.Drawing.Size(370, 54);
+            this.User_Name.TabIndex = 24;
+            this.User_Name.TextAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            // 
+            // Welcome_Text
+            // 
+            this.Welcome_Text.BorderColorFocused = System.Drawing.Color.White;
+            this.Welcome_Text.BorderColorIdle = System.Drawing.Color.White;
+            this.Welcome_Text.BorderColorMouseHover = System.Drawing.Color.White;
+            this.Welcome_Text.BorderThickness = 3;
+            this.Welcome_Text.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.Welcome_Text.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Welcome_Text.ForeColor = System.Drawing.Color.White;
+            this.Welcome_Text.isPassword = false;
+            this.Welcome_Text.Location = new System.Drawing.Point(431, 272);
+            this.Welcome_Text.Margin = new System.Windows.Forms.Padding(7);
+            this.Welcome_Text.Name = "Welcome_Text";
+            this.Welcome_Text.Size = new System.Drawing.Size(437, 50);
+            this.Welcome_Text.TabIndex = 23;
+            this.Welcome_Text.Text = "SMART MOVIE THEATER MANAGEMENT";
+            this.Welcome_Text.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // Login_Button
+            // 
+            this.Login_Button.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.Login_Button.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.Login_Button.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.Login_Button.BorderRadius = 0;
+            this.Login_Button.ButtonText = "Login";
+            this.Login_Button.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Login_Button.DisabledColor = System.Drawing.Color.Gray;
+            this.Login_Button.Iconcolor = System.Drawing.Color.Transparent;
+            this.Login_Button.Iconimage = null;
+            this.Login_Button.Iconimage_right = null;
+            this.Login_Button.Iconimage_right_Selected = null;
+            this.Login_Button.Iconimage_Selected = null;
+            this.Login_Button.IconMarginLeft = 0;
+            this.Login_Button.IconMarginRight = 0;
+            this.Login_Button.IconRightVisible = true;
+            this.Login_Button.IconRightZoom = 0D;
+            this.Login_Button.IconVisible = true;
+            this.Login_Button.IconZoom = 90D;
+            this.Login_Button.IsTab = false;
+            this.Login_Button.Location = new System.Drawing.Point(536, 541);
+            this.Login_Button.Name = "Login_Button";
+            this.Login_Button.Normalcolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.Login_Button.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(36)))), ((int)(((byte)(129)))), ((int)(((byte)(77)))));
+            this.Login_Button.OnHoverTextColor = System.Drawing.Color.LightSalmon;
+            this.Login_Button.selected = false;
+            this.Login_Button.Size = new System.Drawing.Size(229, 48);
+            this.Login_Button.TabIndex = 22;
+            this.Login_Button.Text = "Login";
+            this.Login_Button.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.Login_Button.Textcolor = System.Drawing.Color.White;
+            this.Login_Button.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Login_Button.Click += new System.EventHandler(this.Login_Button_Click);
+            // 
+            // Password_Icon
+            // 
+            this.Password_Icon.BackColor = System.Drawing.Color.White;
+            this.Password_Icon.Image = ((System.Drawing.Image)(resources.GetObject("Password_Icon.Image")));
+            this.Password_Icon.Location = new System.Drawing.Point(431, 469);
+            this.Password_Icon.Name = "Password_Icon";
+            this.Password_Icon.Size = new System.Drawing.Size(60, 54);
+            this.Password_Icon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Password_Icon.TabIndex = 21;
+            this.Password_Icon.TabStop = false;
+            // 
+            // User_Icon
+            // 
+            this.User_Icon.BackColor = System.Drawing.Color.White;
+            this.User_Icon.Image = ((System.Drawing.Image)(resources.GetObject("User_Icon.Image")));
+            this.User_Icon.Location = new System.Drawing.Point(431, 397);
+            this.User_Icon.Name = "User_Icon";
+            this.User_Icon.Size = new System.Drawing.Size(60, 54);
+            this.User_Icon.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.User_Icon.TabIndex = 20;
+            this.User_Icon.TabStop = false;
+            // 
+            // Main_Logo
+            // 
+            this.Main_Logo.Image = ((System.Drawing.Image)(resources.GetObject("Main_Logo.Image")));
+            this.Main_Logo.Location = new System.Drawing.Point(498, 12);
+            this.Main_Logo.Name = "Main_Logo";
+            this.Main_Logo.Size = new System.Drawing.Size(319, 239);
+            this.Main_Logo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Main_Logo.TabIndex = 19;
+            this.Main_Logo.TabStop = false;
+            // 
+            // User_Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(5)))), ((int)(((byte)(25)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(1264, 681);
+            this.Controls.Add(this.Admin_Login_Button);
+            this.Controls.Add(this.User_Login_Button);
+            this.Controls.Add(this.Exit_Button);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.User_Name);
+            this.Controls.Add(this.Welcome_Text);
+            this.Controls.Add(this.Login_Button);
+            this.Controls.Add(this.Password_Icon);
+            this.Controls.Add(this.User_Icon);
+            this.Controls.Add(this.Main_Logo);
+            this.Name = "User_Login";
+            this.Text = "User_Login";
+            ((System.ComponentModel.ISupportInitialize)(this.Exit_Button)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Password_Icon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.User_Icon)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Main_Logo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private Bunifu.Framework.UI.BunifuFlatButton Admin_Login_Button;
+        private Bunifu.Framework.UI.BunifuMetroTextbox User_Login_Button;
+        private Bunifu.Framework.UI.BunifuImageButton Exit_Button;
+        private Bunifu.Framework.UI.BunifuMetroTextbox Password;
+        private Bunifu.Framework.UI.BunifuMetroTextbox User_Name;
+        private Bunifu.Framework.UI.BunifuMetroTextbox Welcome_Text;
+        private Bunifu.Framework.UI.BunifuFlatButton Login_Button;
+        private System.Windows.Forms.PictureBox Password_Icon;
+        private System.Windows.Forms.PictureBox User_Icon;
+        private System.Windows.Forms.PictureBox Main_Logo;
+    }
+}
+
